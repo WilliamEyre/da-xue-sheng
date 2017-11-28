0a225edcb33f933c2c3d5cc754acd8772a81121b
WilliamEyre <1764750372@qq.com>
2017-11-29 07:43:37 +0800

    2DAy

---
+++ DaXiongMaozaixian/app/src/main/java/com/example/lenovo/daxiongmaozaixian/app/App.java
@@ -0,0 +1,15 @@
+package com.example.lenovo.daxiongmaozaixian.app;
+
+import android.app.Application;
+
+import com.example.lenovo.daxiongmaozaixian.base.BaseActivity;
+import com.example.lenovo.daxiongmaozaixian.base.BaseFragment;
+
+
+/**
+ * Created by laptop on 2017/11/27.
+ */
+public class App extends Application {
+    public static BaseActivity baseActivity;
+    public static BaseFragment baseFragment;
+}
---
+++ DaXiongMaozaixian/app/src/main/java/com/example/lenovo/daxiongmaozaixian/base/BaseModel.java
@@ -0,0 +1,7 @@
+package com.example.lenovo.daxiongmaozaixian.base;
+
+/**
+ * Created by laptop on 2017/11/27.
+ */
+public interface BaseModel {
+}
